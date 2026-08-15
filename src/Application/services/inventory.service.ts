e2f1a1ede13f2e0b173d8834572c6d6d7585389f
oscarMolina1523 <oscarmolina1523@gmail.com>
2026-08-14 20:22:58 -0600

    feat: add new operations in inventory service to control data

--- src/Application/services/inventory.service.ts
+++ src/Application/services/inventory.service.ts
@@ -12,45 +12,95 @@ export class InventoryService implements IInventoryService {
   constructor(@inject("IInventoryRepository") repository: IInventoryRepository) {
     this._inventoryRepository = repository;
   }
-  
+
   async findAll(page: number = 1, pageSize: number = 100): Promise<Inventory[]> {
     return await this._inventoryRepository.findAll(page, pageSize);
   }
-  
-  async findById(id: string) : Promise<Inventory | null> {
+
+  async findById(id: string): Promise<Inventory | null> {
     return await this._inventoryRepository.findById(id);
   }
-  
+
   async create(data: InventoryDto): Promise<Inventory> {
     const newData: Inventory = new Inventory({
       ...data,
-      id: generateId(), 
+      id: generateId(),
     })
 
     await this._inventoryRepository.create(newData);
     return newData;
   }
 
+  //este update lo usare para modificar el stock minimo
   async update(id: string, data: InventoryDto): Promise<Inventory | null> {
     const existing = await this._inventoryRepository.findById(id);
     if (!existing) {
       return null;
     }
 
-    const newData: Inventory = new Inventory({
-      ...data,
-      id,
-    })
-    
-    await this._inventoryRepository.update(newData);
-    return newData;
+    // const newData: Inventory = new Inventory({
+    //   ...data,
+    //   id,
+    // })
+
+    //le decimos que el actual modelo de existencia va
+    //tener el minimum stock que se esta mandando en la 
+    //peticion
+    existing.minimumStock = data.minimumStock;
+
+    await this._inventoryRepository.update(existing);
+    return existing;
   }
 
-  async delete(id: string) : Promise<void> {
+  async delete(id: string): Promise<void> {
     const existing = await this._inventoryRepository.findById(id);
     if (!existing) {
-      return ;
+      return;
     }
     return await this._inventoryRepository.delete(existing);
   }
+
+  async increaseStock(
+    id: string,
+    quantity: number
+  ): Promise<Inventory | null> {
+
+    const inventory =
+      await this._inventoryRepository.findById(id);
+
+    if (!inventory) {
+      return null;
+    }
+
+    // DOMAIN (en memoria hacer la suma)
+    inventory.increase(quantity);
+
+    // PERSISTENCIA (peticion a base de datos)
+    await this._inventoryRepository.update(inventory);
+
+    return inventory;
+  }
+
+  async decreaseStock(
+    id: string,
+    quantity: number
+  ): Promise<Inventory | null> {
+
+    const inventory =
+      await this._inventoryRepository.findById(id);
+
+    if (!inventory) {
+      return null;
+    }
+
+    // DOMAIN (en memoria hacer la resta)
+    inventory.decrease(quantity);
+
+    // PERSISTENCIA (hacer la peticion a la base de datos)
+    await this._inventoryRepository.update(inventory);
+
+    return inventory;
+  }
 }
+
+
