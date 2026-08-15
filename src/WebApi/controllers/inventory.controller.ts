099899060708f594bc458934af3fca8fe5ae0e8c
oscarMolina1523 <oscarmolina1523@gmail.com>
2026-08-14 20:40:03 -0600

    feat: add new controllers and routes

--- src/WebApi/controllers/inventory.controller.ts
+++ src/WebApi/controllers/inventory.controller.ts
@@ -11,33 +11,61 @@ export class InventoryController {
     this._inventoryService = service;
   }
 
-  getAll= async (req: Request, res: Response)=> {
+  getAll = async (req: Request, res: Response) => {
     const page = Number(req.query.page) || 1;
     const pageSize = Number(req.query.pageSize) || 100;
     const result = await this._inventoryService.findAll(page, pageSize);
     res.json(result);
   }
 
-  create= async (req: Request, res: Response)=> {
+  create = async (req: Request, res: Response) => {
     const result = await this._inventoryService.create(req.body);
     res.status(201).json(result);
   }
 
-  getById= async (req: Request, res: Response)=> {
+  getById = async (req: Request, res: Response) => {
     const id = req.params.id as string;
     const result = await this._inventoryService.findById(id);
     res.json(result);
   }
 
-  update= async (req: Request, res: Response)=> {
+  update = async (req: Request, res: Response) => {
     const id = req.params.id as string;
     const result = await this._inventoryService.update(id, req.body);
     res.json(result);
   }
 
-  delete= async (req: Request, res: Response)=> {
+  delete = async (req: Request, res: Response) => {
     const id = req.params.id as string;
     await this._inventoryService.delete(id);
     res.status(204).send();
   }
+
+  increaseStock = async (req: Request, res: Response) => {
+
+    const id = req.params.id as string;
+    const { quantity } = req.body;
+
+    const result =
+      await this._inventoryService.increaseStock(
+        id,
+        quantity
+      );
+
+    res.json(result);
+  };
+
+  decreaseStock = async (req: Request, res: Response) => {
+
+    const id = req.params.id as string;
+    const { quantity } = req.body;
+
+    const result =
+      await this._inventoryService.decreaseStock(
+        id,
+        quantity
+      );
+
+    res.json(result);
+  };
 }
--- src/WebApi/routes/inventory.routes.ts
+++ src/WebApi/routes/inventory.routes.ts
@@ -5,10 +5,15 @@ import { InventoryController } from "../controllers/inventory.controller";
 const router = express.Router();
 const controller = container.resolve(InventoryController);
 
+//CRUD operations
 router.post("/", controller.create);
 router.get("/", controller.getAll);
 router.get("/:id", controller.getById);
 router.put("/:id", controller.update);
 router.delete("/:id", controller.delete);
+
+// Business operations
+router.post("/:id/increase", controller.increaseStock);
+router.post("/:id/decrease", controller.decreaseStock);
 
 export default router;
