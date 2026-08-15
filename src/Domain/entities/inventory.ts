dc0fd5c2af41c1eb7f72943a1c2910f6b17020a6
oscarMolina1523 <oscarmolina1523@gmail.com>
2026-08-14 20:03:50 -0600

    feat: add methods to increase or decrease the current stock

--- src/Domain/entities/inventory.ts
+++ src/Domain/entities/inventory.ts
@@ -21,4 +21,40 @@ export default class Inventory extends BaseModel {
     this.currentStock = currentStock;
     this.minimumStock = minimumStock;
   }
+
+  //obtener cantidad de stock actual
+  get quantity(): number {
+    return this.currentStock;
+  }
+
+  //incrementar la cantidad de stock qeu tenemos
+  increase(quantity: number): void {
+
+    if (quantity <= 0) {
+      throw new Error(
+        "La cantidad debe ser mayor que cero"
+      );
+    }
+
+    this.currentStock += quantity;
+  }
+
+  //disminuir la cantidad de stock
+  decrease(quantity: number): void {
+
+    if (quantity <= 0) {
+      throw new Error(
+        "La cantidad debe ser mayor que cero"
+      );
+    }
+
+    if (quantity > this.currentStock) {
+      throw new Error(
+        "No hay suficiente inventario"
+      );
+    }
+
+    this.currentStock -= quantity;
+  }
+
 }
