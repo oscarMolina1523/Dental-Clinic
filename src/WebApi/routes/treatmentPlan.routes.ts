9642179d7068b85eb9b50bcf72e1462209493a35
oscarMolina1523 <oscarmolina1523@gmail.com>
2026-08-15 20:49:01 -0600

    feat: add methods in controller and routes

--- src/WebApi/routes/treatmentPlan.routes.ts
+++ src/WebApi/routes/treatmentPlan.routes.ts
@@ -11,4 +11,15 @@ router.get("/:id", controller.getById);
 router.put("/:id", controller.update);
 router.delete("/:id", controller.delete);
 
+router.post("/:id/propose", controller.propose);
+router.post("/:id/accept", controller.accept);
+router.post("/:id/start", controller.start);
+router.post("/:id/complete", controller.complete);
+router.post("/:id/cancel", controller.cancel);
+
+router.put("/:id/subtotal", controller.setSubtotal);
+
+router.put("/:id/discount", controller.applyDiscount);
+router.delete("/:id/discount", controller.removeDiscount);
+
 export default router;
