1df6331a3d538f3a0ce9274563ccf2c302369b8b
oscarMolina1523 <oscarmolina1523@gmail.com>
2026-08-15 18:58:54 -0600

    feat: add methods in controller and new routes for patients

--- src/WebApi/routes/patient.routes.ts
+++ src/WebApi/routes/patient.routes.ts
@@ -11,4 +11,43 @@ router.get("/:id", controller.getById);
 router.put("/:id", controller.update);
 router.delete("/:id", controller.delete);
 
+router.put(
+  "/:id/phone",
+  controller.changePhoneNumber
+);
+
+router.put(
+  "/:id/email",
+  controller.changeEmail
+);
+
+router.put(
+  "/:id/address",
+  controller.changeAddress
+);
+
+// Contacto de emergencia
+router.put(
+  "/:id/emergency-contact",
+  controller.updateEmergencyContact
+);
+
+// Imagen
+router.put(
+  "/:id/image",
+  controller.changeImage
+);
+
+// Estado
+router.post(
+  "/:id/activate",
+  controller.activate
+);
+
+router.post(
+  "/:id/deactivate",
+  controller.deactivate
+);
+
+
 export default router;
