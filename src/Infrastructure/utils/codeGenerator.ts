2fc7e3e6c0f22f129460ce0e1b67419e3a795252
oscarMolina1523 <oscarmolina1523@gmail.com>
2026-09-13 14:17:08 -0600

    feat: add seconds , minutes and hours to generate unique id

--- src/Infrastructure/utils/codeGenerator.ts
+++ src/Infrastructure/utils/codeGenerator.ts
@@ -32,7 +32,13 @@ export function generateEntityCode({
     const year = d.getFullYear();
     const month = String(d.getMonth() + 1).padStart(2, "0");
     const day = String(d.getDate()).padStart(2, "0");
-    parts.push(`${year}${month}${day}`);
+    const hours = String(d.getHours()).padStart(2, "0");
+
+    const minutes = String(d.getMinutes()).padStart(2, "0");
+
+    const seconds = String(d.getSeconds()).padStart(2, "0");
+
+    return `${prefix}-${year}${month}${day}-${hours}${minutes}${seconds}-${uniqueId}`;
   }
 
   // Identificador único limpio (quita caracteres especiales)
